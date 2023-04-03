6da1f20e87ee99d00d50b41ec9a7b6cf81ed5e40
bcstidd <bc.stidd@gmail.com>
2023-04-02 22:57:00 -0700

    EOD

--- gulpfile.js
+++ gulpfile.js
@@ -37,10 +37,8 @@ gulp.task("html:build", function () {
     )
     .pipe(
       comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
+    LINKEDIN: https://www.linkedin.com/in/brittany-stidd
+    GITHUB: https://github.com/bc.stidd/
     `)
     )
     .pipe(gulp.dest(path.build.dirDev))
@@ -65,7 +63,8 @@ gulp.task("scss:build", function () {
     .pipe(sourcemaps.write("/"))
     .pipe(
       comments(`
-
+   
+    LINKEDIN: https://www.linkedin.com/in/brittany-stidd
     GITHUB: https://github.com/bc.stidd/
     `)
     )
@@ -83,10 +82,9 @@ gulp.task("js:build", function () {
     .src(path.src.js)
     .pipe(
       comments(`
-  WEBSITE: https://themefisher.com
-  TWITTER: https://twitter.com/themefisher
-  FACEBOOK: https://www.facebook.com/themefisher
-  GITHUB: https://github.com/themefisher/
+  
+  LINKEDIN: https://www.linkedin.com/in/brittany-stidd
+  GITHUB: https://github.com/bcstidd/
   `)
     )
     .pipe(gulp.dest(path.build.dirDev + "js/"))

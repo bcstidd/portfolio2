be879cbe8893da19dc754221574b64538ff4e2c4
bcstidd <bc.stidd@gmail.com>
2023-04-05 17:05:01 -0700

    adds tech logos in slick-slider

--- theme/js/script.js
+++ theme/js/script.js
@@ -105,41 +105,42 @@
     // clients logo slider
     $('.client-logo-slider').slick({
         infinite: true,
-        slidesToShow: 5,
+        slidesToShow: 7,
         slidesToScroll: 1,
         autoplay: true,
         dots: false,
         arrows: false,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 400,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
             }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
         ]
-    });
+      });
 
     // Shuffle js filter and masonry
     var Shuffle = window.Shuffle;
